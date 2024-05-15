17dbca3f42b40e92b56ce6dfdb1cf65bbe5dbf26
MohdSanah4078 <270167114@yoobeestudent.ac.nz>
2024-05-15 13:26:04 +1200

    initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwords.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Password(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    website = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+# Create all database tables before the first request is handled
+@app.before_request
+def before_request():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You have successfully signed up!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Login successful!', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash('Incorrect password. Please try again.', 'danger')
+        else:
+            flash('User not found. Please sign up.', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    flash('You have been logged out', 'info')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
